69b6d599e88d9fb164b1c6ac70b9fd5ecf4d7891
ngocpmt <np1552005@gmail.com>
2026-05-26 22:28:13 +0700

    chore: config astro and add CI script for Gitlab Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,4 +7,12 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+  site: "https://emercard.id.vn",
+  i18n: {
+    defaultLocale: "vi",
+    locales: ["vi", "en"],
+    routing: {
+      prefixDefaultLocale: false,
+    },
+  },
 });
